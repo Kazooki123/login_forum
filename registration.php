890a7da3e2738f67a57be23334517fe079c3b9fa
KAZOOKIovestocode <139462470+Kazooki123@users.noreply.github.com>
2024-03-11 19:04:50 +0800

    Update #4

--- registration.php
+++ registration.php
@@ -13,7 +13,7 @@ if (isset($_SESSION["users"])) {
     <meta name="viewpoint" content="width-device-width, initial-scale=1.0">
     <title>Registration Form</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href="../public/style.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <div class="container">
